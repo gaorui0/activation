f032d06073a438945e130a24f14df4e2247c4ce4
gaorui0 <58246763+gaorui0@users.noreply.github.com>
2020-07-01 15:12:05 +0800

    Create JH.py

---
+++ JH.py
@@ -0,0 +1,15 @@
+import os
+import time
+
+
+tem1 = os.system('slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX')
+print(tem1)
+time.sleep(5)
+tem2 = os.system('slmgr /skms  192.168.253.50')
+print(tem2)
+time.sleep(5)
+tem3 = os.system('slmgr /ato')
+print(tem3)
+
+
+#winsows10专业版激活
